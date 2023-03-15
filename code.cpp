8a014ef74d6fa6369165dffdc08b537bd49ff741
dvalere <127788870+dvalere@users.noreply.github.com>
2023-03-15 02:14:03 +0000

    rn

--- code.cpp
+++ code.cpp
@@ -5,10 +5,22 @@
 #include <cmath>
 using namespace std;
 
+void evaluate_cholesterol(int, int, string&, string&);
+void evaluate_blood_pressure(int, int, string&, string&);
+
 int main()
 {
-    
-cout << "test";
-cout << "test2";
+    int numbah;
+    int counter = 0;
+    // Setting up variables 
+
+    cout << "How many patient's records do you wish to read? (Only 4 are available)" << endl;
+    cin >> numbah;
+    /* Asking the person how many people's records they want to see, so the program can show them how many they choose. 
+    numbah is the amount they choose. */
+
+    while (counter < numbah){
+        
+    }
 
 }
